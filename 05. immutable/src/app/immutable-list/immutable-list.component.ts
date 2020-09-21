b7bdd80c56ba29ae8aaadcfc7228975c1a9e8d36
alex-31g <al.tsimbalyuk@gmail.com>
2020-09-21 19:07:23 +0300

    update immutable.js

--- 05. immutable/src/app/immutable-list/immutable-list.component.ts
+++ 05. immutable/src/app/immutable-list/immutable-list.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from 'immutable';
 
 @Component({
-  selector: 'immutable-list',
+  selector: 'app-immutable-list',
   templateUrl: './immutable-list.component.html',
 })
 export class ImmutableListComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    console.log('======= List =======');
+
+    // Создаем пустой immutable массив с помощью метода List
+    let list = List<Video>();
+
+    // push - возвращает новый массив с добавленным значением
+    list = list.push(new Video(100, 'Angular Essential'));
+    list = list.push(new Video(101, 'Angular Advanced'));
+    list = list.push(new Video(102, 'Angular CLI'));
+
+    // ф-ция count() входит в состав метода List
+    console.log('list.count ' + list.count());
+  }
+}
+class Video {
+  constructor(public id: number, public name: string) {}
 }
